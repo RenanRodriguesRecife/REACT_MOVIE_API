62daacd1be032c12bac0ae795b5bfd0d957a42c4
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-04-13 20:19:25 -0300

    base home page

--- api-filmes/src/pages/home/index.js
+++ api-filmes/src/pages/home/index.js
@@ -1,6 +1,23 @@
 function Home(){
     return(
-        <h1>Hello, World!</h1>
+        <>
+            <h1>Movies</h1>
+            <ul>
+                <li>
+                    <img src="https://http2.mlstatic.com/D_NQ_NP_866792-MLB32711553663_102019-O.jpg" alt="spiderman"></img>
+                    <span>Spider man</span>
+                </li>
+                <li>
+                    <img src="https://http2.mlstatic.com/D_NQ_NP_866792-MLB32711553663_102019-O.jpg" alt="spiderman"></img>
+                    <span>Spider man</span>
+                </li>
+                <li>
+                    <img src="https://http2.mlstatic.com/D_NQ_NP_866792-MLB32711553663_102019-O.jpg" alt="spiderman"></img>
+                    <span>Spider man</span>
+                </li>
+            </ul>
+        </>
+        
     )
 }
 
