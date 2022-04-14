7d510cabfae76062628cc9bdac2e899420ea7e53
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-04-14 08:41:14 -0300

    dinamic test

--- api-filmes/src/pages/home/index.js
+++ api-filmes/src/pages/home/index.js
@@ -1,22 +1,34 @@
 import { Container, MoveList, Movie } from "./styles";
 
 function Home(){
+
+    const movies = [
+        {
+            title: 'blade runner',
+            image_url: 'https://http2.mlstatic.com/D_NQ_NP_866792-MLB32711553663_102019-O.jpg',
+        },
+        {
+            title: 'blade runner',
+            image_url: 'https://http2.mlstatic.com/D_NQ_NP_866792-MLB32711553663_102019-O.jpg',
+        },
+        {
+            title: 'blade runner',
+            image_url: 'https://http2.mlstatic.com/D_NQ_NP_866792-MLB32711553663_102019-O.jpg',
+        }
+    ]
+
     return(
         <Container>
             <h1>Movies</h1>
             <MoveList>
-                <Movie>
-                    <a href="#"><img src="https://http2.mlstatic.com/D_NQ_NP_866792-MLB32711553663_102019-O.jpg" alt="spiderman"></img></a>
-                    <span>Spider man</span>
-                </Movie>
-                <Movie>
-                    <a href="#"><img src="https://http2.mlstatic.com/D_NQ_NP_866792-MLB32711553663_102019-O.jpg" alt="spiderman"></img></a>
-                    <span>Spider man</span>
-                </Movie>
-                <Movie>
-                    <a href="#"><img src="https://http2.mlstatic.com/D_NQ_NP_866792-MLB32711553663_102019-O.jpg" alt="spiderman"></img></a>
-                    <span>Spider man</span>
-                </Movie>
+                {movies.map(movie => {
+                    return(
+                        <Movie>
+                            <a href="#"><img src={movie.image_url} alt={movie.title}></img></a>
+                            <span>{movie.title}</span>
+                        </Movie>
+                )
+                })}
             </MoveList>
         </Container>
         
