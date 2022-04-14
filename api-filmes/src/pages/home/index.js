ae45c98b68740f49e351e5317532002d599797db
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-04-14 08:45:23 -0300

    add key value

--- api-filmes/src/pages/home/index.js
+++ api-filmes/src/pages/home/index.js
@@ -4,14 +4,32 @@ function Home(){
 
     const movies = [
         {
+            id: 1,
             title: 'blade runner',
             image_url: 'https://http2.mlstatic.com/D_NQ_NP_866792-MLB32711553663_102019-O.jpg',
         },
         {
+            id: 2,
             title: 'blade runner',
             image_url: 'https://http2.mlstatic.com/D_NQ_NP_866792-MLB32711553663_102019-O.jpg',
         },
         {
+            id: 3,
+            title: 'blade runner',
+            image_url: 'https://http2.mlstatic.com/D_NQ_NP_866792-MLB32711553663_102019-O.jpg',
+        },
+        {
+            id: 1,
+            title: 'blade runner',
+            image_url: 'https://http2.mlstatic.com/D_NQ_NP_866792-MLB32711553663_102019-O.jpg',
+        },
+        {
+            id: 2,
+            title: 'blade runner',
+            image_url: 'https://http2.mlstatic.com/D_NQ_NP_866792-MLB32711553663_102019-O.jpg',
+        },
+        {
+            id: 3,
             title: 'blade runner',
             image_url: 'https://http2.mlstatic.com/D_NQ_NP_866792-MLB32711553663_102019-O.jpg',
         }
@@ -23,7 +41,7 @@ function Home(){
             <MoveList>
                 {movies.map(movie => {
                     return(
-                        <Movie>
+                        <Movie key={movie.id}>
                             <a href="#"><img src={movie.image_url} alt={movie.title}></img></a>
                             <span>{movie.title}</span>
                         </Movie>
