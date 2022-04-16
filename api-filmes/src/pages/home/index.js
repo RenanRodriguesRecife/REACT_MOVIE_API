4f00a668194e53e1354270da575707ea94494857
Renan Rodrigues <renan_ime_2012@hotmail.com>
2022-04-16 00:58:23 -0300

    create routes

--- api-filmes/src/pages/home/index.js
+++ api-filmes/src/pages/home/index.js
@@ -1,6 +1,7 @@
 import { Container, MoveList, Movie } from "./styles";
 import { useState, useEffect } from 'react'
 import { APIKey } from '../../config/key';
+import { Link } from 'react-router-dom'
 
 const image_path = 'https://image.tmdb.org/t/p/w500/'
 
@@ -23,7 +24,7 @@ function Home(){
                 {movies?.map(movie => {
                     return(
                         <Movie key={movie.id}>
-                <a href="#"><img src={`${image_path}/${movie.poster_path}`} alt={movie.title}></img></a>
+                <Link to={`/details/${movie.id}`}><img src={`${image_path}/${movie.poster_path}`} alt={movie.title}></img></Link>
                             <span>{movie.title}</span>
                         </Movie>
                     )
